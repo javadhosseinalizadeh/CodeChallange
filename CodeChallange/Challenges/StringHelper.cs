10ca2b4fda49b15e8c619cc58e76f7fd566bfe41
Javad <javad@example.com>
2025-05-19 14:37:36 +0330

    ReverseString

---
+++ CodeChallange/Challenges/StringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallange.Challenges
+{
+    public static class StringHelper
+    {
+        public static string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            var chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
--- CodeChallange/Program.cs
+++ CodeChallange/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodeChallange.Challenges;
+
+Console.Title = "Code Challenge App (.NET 8)";
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("==== Code Challenge Menu ====");
+    Console.WriteLine("1. Reverse a String");
+    Console.WriteLine("2. Palindrome Checker");
+    Console.WriteLine("3. Remove Duplicates");
+    Console.WriteLine("4. FizzBuzz");
+    Console.WriteLine("5. Group Words by First Letter");
+    Console.WriteLine("6. Password Strength Validator");
+    Console.WriteLine("7. LRU Cache Demo");
+    Console.WriteLine("0. Exit");
+    Console.Write("\nEnter your choice: ");
+
+    var input = Console.ReadLine();
+
+    Console.Clear();
+    switch (input)
+    {
+        case "1":
+            Console.Write("Enter string: ");
+            var str = Console.ReadLine();
+            Console.WriteLine("Reversed: " + StringHelper.ReverseString(str!));
+            break;
+
+        
+
+        case "0":
+            return;
+
+        default:
+            Console.WriteLine("Invalid choice. Try again.");
+            break;
+    }
+
+    Console.WriteLine("\nPress any key to return to menu...");
+    Console.ReadKey();
+}
