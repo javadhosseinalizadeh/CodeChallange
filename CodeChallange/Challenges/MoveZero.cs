ac594ea7f3c6b080bbb71c092c1d8bc8285e521d
Javad <j.alizadeh1993@yahoo.com>
2026-05-25 00:11:03 +0330

    MoveZero

---
+++ CodeChallange/Challenges/MoveZero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallange.Challenges
+{
+    public class MoveZero
+    {
+        public static int[] MoveZeroToEnd(int[] nums)
+        {
+            if (nums == null || nums.Length < 2)
+                return nums;
+
+            int insertPos = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[insertPos] = nums[i];
+                    insertPos++;
+                }
+            }
+
+            while (insertPos < nums.Length)
+            {
+                nums[insertPos] = 0;
+                insertPos++;
+            }
+
+            return nums;
+        }
+    }
+}
